4efd2791c45468893f84d29b3860d4ba40b1ebb9
wangyi111 <wangyistutt@gmail.com>
2025-03-21 15:43:04 +0100

    reorganize bigearthnet-s1/2

--- Copernicus-Bench/src/datasets/cobench_bigearthnets12_wrapper.py
+++ Copernicus-Bench/src/datasets/cobench_bigearthnets12_wrapper.py
@@ -9,8 +9,8 @@ import rasterio
 import torch
 from torch import Generator, Tensor
 from torch.utils.data import random_split
-from torchgeo.datasets import BigEarthNet
-#from torchgeo.datasets.geo import NonGeoDataset
+#from torchgeo.datasets import BigEarthNet
+from torchgeo.datasets.geo import NonGeoDataset
 
 from pyproj import Transformer
 from datetime import date
@@ -19,7 +19,7 @@ import pdb
 import ast
 
 
-class CoBenchBigEarthNetS12(BigEarthNet):
+class CoBenchBigEarthNetS12(NonGeoDataset):
     url = 'https://huggingface.co/datasets/wangyi111/Copernicus-Bench/resolve/main/l2_bigearthnet_s1s2/bigearthnetv2.zip'
     splits = ('train', 'val', 'test')
     label_filenames = {
@@ -35,50 +35,40 @@ class CoBenchBigEarthNetS12(BigEarthNet):
         self,
         root: str = "data",
         split: str = "train",
-        bands: str = "all",
+        modality: str = "s2",
         band_names: Sequence[str] = ('B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B11', 'B12'),
-        num_classes: int = 19,
         transforms: Optional[Callable[[dict[str, Tensor]], dict[str, Tensor]]] = None,
         download: bool = False,
         checksum: bool = False,
     ) -> None:
 
         assert split in self.splits
-        assert bands in ['s1', 's2']
-        assert num_classes in [43, 19]
+        assert modality in ['s1', 's2']
         self.root = root
         self.split = split
-        self.bands = bands
-        self.num_classes = num_classes
+        self.modality = modality
         self.transforms = transforms
 
 
         self.band_names = band_names
-        if self.bands == 's1':
+        if self.modality == 's1':
             self.all_band_names = self.all_band_names_s1
         else:
             self.all_band_names = self.all_band_names_s2
         self.band_indices = [(self.all_band_names.index(b)+1) for b in band_names if b in self.all_band_names]
 
-        self.class2idx_43 = {c: i for i, c in enumerate(self.class_sets[43])}
-        self.class2idx_19 = {c: i for i, c in enumerate(self.class_sets[19])}
-        #self._verify()
-
-        #self.folders = self._load_folders()
         self.img_paths = []
         self.labels = []
-        self.csv = pd.read_csv(os.path.join(self.root,self.label_filenames[self.split]))
-        for i, row in self.csv.iterrows():
-            if self.bands == 's1':
-                s1_path = os.path.join(self.root, 'BigEarthNet-S1-5%', row['s1_path'])
-                self.img_paths.append(s1_path)
-            elif self.bands == 's2':
-                s2_path = os.path.join(self.root, 'BigEarthNet-S2-5%', row['s2_path'])
-                self.img_paths.append(s2_path)
-            labels = row['labels']
-            labels_list = ast.literal_eval(labels)
-            self.labels.append(labels_list)
 
+        df = pd.read_csv(os.path.join(self.root,self.label_filenames[self.split]))
+        s1_paths = df["s1_path"].tolist()
+        s2_paths = df["s2_path"].tolist()
+        if self.modality == 's1':
+            self.img_paths = [os.path.join(self.root, 'BigEarthNet-S1-5%', path) for path in s1_paths]
+        elif self.modality == 's2':
+            self.img_paths = [os.path.join(self.root, 'BigEarthNet-S2-5%', path) for path in s2_paths]
+        self.class_names = df.columns[2:]  # Exclude 's1_path' and 's2_path'
+        self.binary_labels = df.iloc[:, 2:].values  # Convert label data to NumPy array
 
 
         self.patch_area = (16*10/1000)**2
@@ -87,20 +77,9 @@ class CoBenchBigEarthNetS12(BigEarthNet):
     def __len__(self):
         return len(self.img_paths)
 
-    def get_class2idx(self, label: str, level=19):
-        assert level == 19 or level == 43, "level must be 19 or 43"
-        return self.class2idx_19[label] if level == 19 else self.class2idx_43[label]
-
     def _load_target(self, index: int) -> Tensor:
-        image_labels = self.labels[index]
-
-        # labels -> indices
-        indices = [
-            self.get_class2idx(label, level=self.num_classes) for label in image_labels
-        ]
-
-        image_target = torch.zeros(self.num_classes, dtype=torch.long)
-        image_target[indices] = 1
+        image_labels = self.binary_labels[index]
+        image_target = torch.from_numpy(image_labels).long()
 
         return image_target
 
@@ -120,7 +99,7 @@ class CoBenchBigEarthNetS12(BigEarthNet):
             else:
                 lon, lat = cx, cy
 
-            if self.bands == 's1':
+            if self.modality == 's1':
                 date_str = path.split('/')[-1].split('_')[4]
             else:
                 date_str = path.split('/')[-1].split('_')[2]
@@ -146,10 +125,10 @@ class CoBenchBigEarthNetS12(BigEarthNet):
 
 class ClsDataAugmentation(torch.nn.Module):
 
-    def __init__(self, split, size, bands, band_stats):
+    def __init__(self, split, size, modality, band_stats):
         super().__init__()
 
-        self.bands = bands
+        self.modality = modality
 
         if band_stats is not None:
             mean = band_stats['mean']
@@ -177,19 +156,16 @@ class ClsDataAugmentation(torch.nn.Module):
     @torch.no_grad()
     def forward(self, sample: dict[str,]):
         """Torchgeo returns a dictionary with 'image' and 'label' keys, but engine expects a tuple."""
-        if self.bands == "rgb":
-            sample["image"] = sample["image"][1:4, ...].flip(dims=(0,))
-            # get in rgb order and then normalization can be applied
         x_out = self.transform(sample["image"]).squeeze(0)
         return x_out, sample["label"], sample["meta"]
 
 
 class ClsDataAugmentationSoftCon(torch.nn.Module):
 
-    def __init__(self, split, size, bands, band_stats):
+    def __init__(self, split, size, modality, band_stats):
         super().__init__()
 
-        self.bands = bands
+        self.modality = modality
 
         if band_stats is not None:
             self.mean = band_stats['mean']
@@ -217,7 +193,7 @@ class ClsDataAugmentationSoftCon(torch.nn.Module):
     @torch.no_grad()
     def forward(self, sample: dict[str,]):
         """Torchgeo returns a dictionary with 'image' and 'label' keys, but engine expects a tuple."""
-        if self.bands == 's1':
+        if self.modality == 's1':
             sample_img = sample["image"]
             ### normalize s1
             self.max_q = torch.quantile(sample_img.reshape(2,-1),0.99,dim=1)      
@@ -233,7 +209,7 @@ class ClsDataAugmentationSoftCon(torch.nn.Module):
                 img = self.normalize(img,self.mean[b],self.std[b])         
                 img_bands.append(img)
             sample_img = torch.stack(img_bands,dim=0) # VV,VH (w,h,c)
-        elif self.bands == 's2':
+        elif self.modality == 's2':
             sample_img = sample["image"]
             img_bands = []
             for b in range(12):
@@ -263,57 +239,47 @@ class CoBenchBigEarthNetS12Dataset:
         self.dataset_config = config
         self.img_size = (config.image_resolution, config.image_resolution)
         self.root_dir = config.data_path
-        self.bands = config.modality
+        self.modality = config.modality
         self.band_names = config.band_names
-        self.num_classes = config.num_classes
         self.band_stats = config.band_stats
         self.norm_form = config.norm_form if 'norm_form' in config else None
-
-        if self.bands == "rgb":
-            # start with rgb and extract later
-            self.input_bands = "s2"
-        else:
-            self.input_bands = self.bands
 
     def create_dataset(self):
 
         if self.norm_form == 'softcon':
             train_transform = ClsDataAugmentationSoftCon(
-                split="train", size=self.img_size, bands=self.bands, band_stats=self.band_stats
+                split="train", size=self.img_size, modality=self.modality, band_stats=self.band_stats
             )
             eval_transform = ClsDataAugmentationSoftCon(
-                split="test", size=self.img_size, bands=self.bands, band_stats=self.band_stats
+                split="test", size=self.img_size, modality=self.modality, band_stats=self.band_stats
             )
         else:
             train_transform = ClsDataAugmentation(
-                split="train", size=self.img_size, bands=self.bands, band_stats=self.band_stats
+                split="train", size=self.img_size, modality=self.modality, band_stats=self.band_stats
             )
             eval_transform = ClsDataAugmentation(
-                split="test", size=self.img_size, bands=self.bands, band_stats=self.band_stats
+                split="test", size=self.img_size, modality=self.modality, band_stats=self.band_stats
             )
 
         dataset_train = CoBenchBigEarthNetS12(
             root=self.root_dir,
-            num_classes=self.num_classes,
             split="train",
-            bands=self.input_bands,
+            modality=self.modality,
             band_names=self.band_names,
             transforms=train_transform,
         )
 
         dataset_val = CoBenchBigEarthNetS12(
             root=self.root_dir,
-            num_classes=self.num_classes,
             split="val",
-            bands=self.input_bands,
+            modality=self.modality,
             band_names=self.band_names,
             transforms=eval_transform,
         )
         dataset_test = CoBenchBigEarthNetS12(
             root=self.root_dir,
-            num_classes=self.num_classes,
             split="test",
-            bands=self.input_bands,
+            modality=self.modality,
             band_names=self.band_names,
             transforms=eval_transform,
         )
